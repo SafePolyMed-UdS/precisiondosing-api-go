90f0e3bf08818efbbf54f9a2b54159d388211030
Dominik Selzer <dominik.selzer@uni-saarland.de>
2025-03-19 10:06:16 +0100

    Fix service user endpoint route

--- api/internal/server/routes.go
+++ api/internal/server/routes.go
@@ -56,6 +56,7 @@ func RegisterAdminRoutes(r *gin.RouterGroup, resourceHandle *handle.ResourceHand
 	authUser.Use(middleware.Authentication(&resourceHandle.AuthCfg), middleware.AdminAccess())
 	{
 		authUser.POST("/users/", c.CreateUser)
+		authUser.POST("/users/service", c.CreateServiceUser)
 		authUser.GET("/users", c.GetUsers)
 		authUser.GET("/users/:email", c.GetUserByEmail)
 		authUser.DELETE("/users/:email", c.DeleteUserByEmail)
